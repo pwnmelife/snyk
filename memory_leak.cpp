32dbdd49ae04c439a85bddade29d7b4164305dca
pwnmelife <64112947+pwnmelife@users.noreply.github.com>
2024-01-17 22:13:36 +0800

    Update memory_leak.cpp

--- memory_leak.cpp
+++ memory_leak.cpp
@@ -1,1 +1,17 @@
-https://github.com/pwnmelife/snyk.git
+#include <windows.h>
+#include <iostream>
+
+int main() 
+{
+    int* pMemoryLeak = new int[10];
+    for (int i = 0; i < 10; ++i) 
+    {
+        pMemoryLeak[i] = i * i;
+    }
+    for (int i = 0; i < 10; ++i) 
+    {
+        std::cout << pMemoryLeak[i] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
